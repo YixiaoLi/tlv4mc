fa23041785e68efa7fca50a1d0b89cc6815cfdb1
mizuno <mizuno@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2008-11-20 14:52:38 +0000

    some script

--- TLV/branches/test/tool/path-class.rb
+++ TLV/branches/test/tool/path-class.rb
@@ -4,7 +4,7 @@
 ARGF.each do |line|
   path,klass = line.split ':',2
   if klass =~ /class\s+([^ <]+(?:<[^>]+>)?)\s+/ then
-    puts %("#{File.dirname(path)}","#{$1.chomp}")
+    puts %("#{File.dirname(path)}","#{$1.gsub(/\s+/,'').chomp}")
   else
     raise 'must not happen'
   end
