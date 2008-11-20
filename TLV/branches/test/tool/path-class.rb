5ef7da6b5f8287e1775ec8a410d08efc11dd636f
mizuno <mizuno@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2008-11-20 10:59:05 +0000

    path-class生成スクリプト

---
+++ TLV/branches/test/tool/path-class.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+# -*- mode:ruby; coding:utf-8 -*-
+
+ARGF.each do |line|
+  path,klass = line.split ':',2
+  if klass =~ /class\s+([^ <]+(?:<[^>]+>)?)\s+/ then
+    puts %("#{File.dirname(path)}","#{$1.chomp}")
+  else
+    raise 'must not happen'
+  end
+end
