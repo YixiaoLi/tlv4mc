dbe013d0d0648cd40d780c808df632df33624a70
yanagisawa <yanagisawa@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2008-12-16 13:17:32 +0000

    完了したもの+自動生成したもの

---
+++ test_081113/Test/Core/FileContext/ResourceData/ResourceDataTest.cs
@@ -0,0 +1,94 @@
+﻿using NU.OJL.MPRTOS.TLV.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace NU.OJL.MPRTOS.TLV.Test
+{
+    
+    
+    /// <summary>
+    ///ResourceDataTest のテスト クラスです。すべての
+    ///ResourceDataTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class ResourceDataTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///ToJson のテスト
+        ///</summary>
+        [TestMethod()]
+        public void ToJsonTest()
+        {
+            ResourceData target = new ResourceData(); // TODO: 適切な値に初期化してください
+            string expected = string.Empty; // TODO: 適切な値に初期化してください
+            string actual;
+            actual = target.ToJson();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("このテストメソッドの正確性を確認します。");
+        }
+
+        /// <summary>
+        ///Parse のテスト
+        ///</summary>
+        [TestMethod()]
+        public void ParseTest()
+        {
+            ResourceData target = new ResourceData(); // TODO: 適切な値に初期化してください
+            string resourceData = string.Empty; // TODO: 適切な値に初期化してください
+            ResourceData expected = null; // TODO: 適切な値に初期化してください
+            ResourceData actual;
+            actual = target.Parse(resourceData);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("このテストメソッドの正確性を確認します。");
+        }
+    }
+}
