7a3809ad629c9f2e0040ff7bf48cf6ff4d56caa7
yanagisawa <yanagisawa@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2008-12-25 03:13:01 +0000

    AreaとCoordinateの単体テスト実装は完了

---
+++ branches/test/test_081113/Test/Core/JsonConverters/AreaConverterTest.cs
@@ -0,0 +1,108 @@
+﻿using NU.OJL.MPRTOS.TLV.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NU.OJL.MPRTOS.TLV.Base;
+using System;
+
+namespace Test
+{
+    
+    
+    /// <summary>
+    ///AreaConverterTest のテスト クラスです。すべての
+    ///AreaConverterTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class AreaConverterTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Type のテスト
+        ///</summary>
+        [TestMethod()]
+        public void TypeTest()
+        {
+            AreaConverter target = new AreaConverter();
+            Type actual;
+            actual = target.Type;
+            Assert.AreEqual(typeof(Area), actual);
+        }
+
+        /// <summary>
+        ///WriteJson のテスト
+        ///</summary>
+        [TestMethod()]
+        public void WriteJsonTest()
+        {
+            AreaConverter target = new AreaConverter(); // TODO: 適切な値に初期化してください
+            IJsonWriter writer = null; // TODO: 適切な値に初期化してください
+            object obj = null; // TODO: 適切な値に初期化してください
+            target.WriteJson(writer, obj);
+            Assert.Inconclusive("値を返さないメソッドは確認できません。");
+        }
+
+        /// <summary>
+        ///ReadJson のテスト
+        ///</summary>
+        [TestMethod()]
+        public void ReadJsonTest()
+        {
+            AreaConverter target = new AreaConverter(); // TODO: 適切な値に初期化してください
+            IJsonReader reader = null; // TODO: 適切な値に初期化してください
+            object expected = null; // TODO: 適切な値に初期化してください
+            object actual;
+            actual = target.ReadJson(reader);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("このテストメソッドの正確性を確認します。");
+        }
+    }
+}
---
+++ branches/test/test_081113/Test/Core/JsonConverters/SizeConverterTest.cs
@@ -0,0 +1,120 @@
+﻿using NU.OJL.MPRTOS.TLV.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NU.OJL.MPRTOS.TLV.Base;
+using System;
+
+namespace Test
+{
+    
+    
+    /// <summary>
+    ///SizeConverterTest のテスト クラスです。すべての
+    ///SizeConverterTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class SizeConverterTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Type のテスト
+        ///</summary>
+        [TestMethod()]
+        public void TypeTest()
+        {
+            SizeConverter target = new SizeConverter();
+            Type actual;
+            actual = target.Type;
+            Assert.AreEqual(typeof(Size), target.Type);
+        }
+
+        private string Write(Size size)
+        {
+            SizeConverter target = new SizeConverter();
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            IJsonWriter writer = new NU.OJL.MPRTOS.TLV.Third.JsonWriter(new Newtonsoft.Json.JsonTextWriter(sw));
+            target.WriteJson(writer, size);
+            return sw.ToString();
+        }
+
+        private Size Read(string s)
+        {
+            SizeConverter target = new SizeConverter();
+            IJsonReader reader = new NU.OJL.MPRTOS.TLV.Third.JsonReader(
+               new Newtonsoft.Json.JsonTextReader(new System.IO.StringReader(s)));
+            reader.Read();
+            return (Size)target.ReadJson(reader);
+        } 
+
+        /// <summary>
+        ///WriteJson のテスト
+        ///</summary>
+        [TestMethod()]
+        public void WriteJsonTest1()
+        {
+            Size size = new Size("1,1");
+            Assert.AreEqual("\"1,1\"", Write(size));
+            size = new Size("-20,0");
+            Assert.AreEqual("\"-20,0\"", Write(size));
+        }
+
+        /// <summary>
+        ///ReadJson のテスト
+        ///</summary>
+        [TestMethod()]
+        public void ReadJsonTest()
+        {
+            Assert.AreEqual(new Size("1", "2").ToString() , Read("\"1,2\"").ToString());
+            Assert.AreEqual(new Size("-20", "foo").ToString(), Read("\"-20,foo\"").ToString());
+        }
+    }
+}
