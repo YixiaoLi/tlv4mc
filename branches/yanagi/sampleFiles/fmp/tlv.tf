a29e920ef1ffbf513df380d7e2ba96f2ce1aeaad
yanagisawa <yanagisawa@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-02-16 15:02:58 +0000

    mzpと作業して解決しなかった時点

--- branches/yanagi/sampleFiles/fmp/tlv.tf
+++ branches/yanagi/sampleFiles/fmp/tlv.tf
@@ -7,13 +7,17 @@ $TAB$"VisualizeRules" :["toppers","fmp","fmp_core$TNUM_PRC$"],$NL$
 $TAB$"ResourceHeaders":["fmp"],$NL$
 $TAB$"Resources":$NL$
 $TAB${$NL$
-$JOINEACH tskid TSK.ID_LIST ",\n"$
-    $TAB$$TAB$"$tskid$":{$NL$
+$JOINEACH prcid RANGE(1, 2) ",\n"$
+    $TAB$$TAB$
+    $JOINEACH tskid TSK.ID_LIST ",\n"$
+    $TAB$$TAB$"CPU$prcid$_$tskid$":{$NL$
     $TAB$$TAB$$TAB$"Type":"Task",$NL$
     $TAB$$TAB$$TAB$"Attributes":$NL$
     $TAB$$TAB$$TAB${$NL$
-    $TAB$$TAB$$TAB$$TAB$"prcId" :$CLASS_AFFINITY_INI[TSK.CLASS[tskid]]$,$NL$
+    $TAB$$TAB$$TAB$$TAB$"prcId" :$prcid$,$NL$
+    $TAB$$TAB$$TAB$$TAB$"initprcId" :$CLASS_AFFINITY_INI[TSK.CLASS[tskid]]$,$NL$
     $TAB$$TAB$$TAB$$TAB$"id"    :$+tskid$,$NL$
+    $TAB$$TAB$$TAB$$TAB$"tskid"    :$+tskid$,$NL$
     $TAB$$TAB$$TAB$$TAB$"atr"   :"$TSK.TSKATR[tskid]$",$NL$
     $TAB$$TAB$$TAB$$TAB$"pri"   :$+TSK.ITSKPRI[tskid]$,$NL$
     $TAB$$TAB$$TAB$$TAB$"exinf" :$TSK.EXINF[tskid]$,$NL$
@@ -21,13 +25,14 @@ $JOINEACH tskid TSK.ID_LIST ",\n"$
     $TAB$$TAB$$TAB$$TAB$"stksz" :$+TSK.STKSZ[tskid]$,$NL$
     $TAB$$TAB$$TAB$$TAB$"stk"   :"NULL",$NL$
     $TAB$$TAB$$TAB$$TAB$"state" :
-        $IF TSK.TSKATR[tskid] == TA_ACT$
+        $IF TSK.TSKATR[tskid] == TA_ACT && prcid == CLASS_AFFINITY_INI[TSK.CLASS[tskid]] $
             "RUNNABLE"$NL$
         $ELSE$
             "DORMANT"$NL$
         $END$
     $TAB$$TAB$$TAB$}$NL$
     $TAB$$TAB$}
+    $END$
 $END$
 $NL$
 $TAB$}$NL$
