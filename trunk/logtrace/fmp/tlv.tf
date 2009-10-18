8ad839ac72af0091301d84ceec2624ee407a6718
honda <honda@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-10-18 14:58:45 +0000

    CPU例外ハンドラの出力に問題があったため修正．

--- trunk/logtrace/fmp/tlv.tf
+++ trunk/logtrace/fmp/tlv.tf
@@ -94,7 +94,7 @@ $JOINEACH excno EXC.ORDER_LIST ",\n"$
     $TAB$$TAB$$TAB$"Attributes":$NL$
     $TAB$$TAB$$TAB${$NL$
     $TAB$$TAB$$TAB$$TAB$"prcIdE" :$CLASS_AFFINITY_INI[EXC.CLASS[excno]]$,$NL$
-    $TAB$$TAB$$TAB$$TAB$"id"    :$+EXC.EXCNO[inhno]-(0x10000)*(CLASS_AFFINITY_INI[INH.CLASS[excno]])$,$NL$
+    $TAB$$TAB$$TAB$$TAB$"id"    :$+EXC.EXCNO[excno]-(0x10000)*(CLASS_AFFINITY_INI[EXC.CLASS[excno]])$,$NL$
     $TAB$$TAB$$TAB$$TAB$"state"    : "DORMANT"$NL$
     $TAB$$TAB$$TAB$}$NL$
     $TAB$$TAB$}
