928561c70e7f24b94591bd19d0626befd17387dd
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-10-26 06:42:27 +0000

    リファクタリング

--- others/json-validator-ichihara/JSON_Validator/Program.cs
+++ others/json-validator-ichihara/JSON_Validator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.IO;
 using Newtonsoft.Json;
@@ -11,66 +10,38 @@ namespace JSON_Validator
 {
     class JsonValidator
     {
-        private String schemaJson = null;   // JSON schema
-        private String trgJson = null;       // 検証するJSONファイル
+        readonly private string schema = null;   // JSON schema
+        readonly private string json = null;       // 検証するJSONファイル
+        public enum Status { Valid, Invalid, JsonError, SchemaError };
 
-        // set*()を実行していれば ture
-        private bool ssflag = false;
-        private bool sjflag = false;
-
-
-        // JSON schemaの内容をString型の変数に格納メソッド
-        public void SetSchema(String path)
+        public JsonValidator(string schemaPath, string jsonPath)
         {
-            try
-            {
-                schemaJson = File.ReadAllText(path);
-            }
-            catch ( Exception e )
-            {
-                errWrite("setSchema", e.GetType().FullName);
-                Environment.Exit(1);
-            }
-
-            ssflag = true;
-        }
-
-        // 検証するJSONファイルの内容をString型の変数に格納するメソッド
-        public void SetJson(String path)
-        {
-            try
-            {
-                trgJson = File.ReadAllText(path);
-            }
-            catch( Exception e )
-            {
-                errWrite("setJson", e.GetType().FullName);
-                Environment.Exit(1);
-            }
-
-            sjflag = true;
+            this.schema = File.ReadAllText(schemaPath);
+            this.json = File.ReadAllText(jsonPath);
         }
 
 
-        public void Run()
+        public Status Run()
         {
-            if (!ssflag && !sjflag)    // set*()をしていない場合
-            {
-                errWrite("run", "set*() has not been called .");  // 英語が怪しい
-                return;
-            }
-
             // 値設定　兼　ill-formed 調査
             JsonSchema schema = SchemaParse();
-            JObject person = JsonParse();
+            JObject json = JsonParse();
 
-            if (person.IsValid(schema)) // 検証   C#ならもっと美しくできる？
+            if (null == schema)
             {
-                Console.Write("Valid\n");
+                return Status.SchemaError;
+            }
+            else if (null == json)
+            {
+                return Status.JsonError;
+            }
+            else if (json.IsValid(schema))
+            {
+                return Status.Valid;
             }
             else
             {
-                Console.Write("Invalid\n");
+                return Status.Invalid;
             }
         }
 
@@ -78,65 +49,63 @@ namespace JSON_Validator
         // 理由：JsonSchemaのill-formedを検出するため。
         private JsonSchema SchemaParse()
         {
-            JsonSchema schema = null;
-
             try
             {
-                schema = JsonSchema.Parse(schemaJson);  // JsonSchemaを設定 -> 細かいことは不明
+                return JsonSchema.Parse(this.schema);  // JsonSchemaを設定 -> 細かいことは不明
             }
-            catch(JsonReaderException e)
+            catch (JsonReaderException e)
             {
-                Console.Write("JSON schema is ill-formed\n"); // 英語が怪しい
-                //Console.Write(e.Message);                     // 誤り箇所の指摘 -> Invalid property identifier character: ?. Line *, position *.
-                Environment.Exit(1);
+                Console.Error.WriteLine(e.StackTrace);
+                return null;
             }
-            return schema;
         }
 
         // 検証するJSONファイルを設定してそのオブジェクトを返すメソッド
         // 検証するJSONファイルの ill-formed を検出するため
         private JObject JsonParse()
         {
-            JObject json = null;
-
             try
             {
-                json = JObject.Parse(trgJson);  // 検証するJSONファイルを設定 -> 細かいことは不明
+                return JObject.Parse(this.json);  // 検証するJSONファイルを設定 -> 細かいことは不明
             }
-            catch(JsonReaderException e)
+            catch (JsonReaderException e)
             {
-                Console.Write("JSON is ill-formed\n"); // 英語が怪しい
-                //Console.Write(e.Message);
-                Environment.Exit(1);
+                Console.Error.WriteLine(e.StackTrace);
+                return null;
             }
-            return json;
         }
-
-        // どのメソッドでどういう例外が飛んだかを標準出力するメソッド
-        // mName: メソッド名
-        // eName: 例外名など
-        private void errWrite(String mName, String eName)
-        {
-            Console.Write("ERROR: {0} : {1}\n", mName, eName);
-        }
-
-
 
         // テスト用メインメソッド
         static void Main(string[] args)
         {
             // ファイルパス
-            String schemaPath = "F:\\test\\test.txt"; // JSON schema
-            String jsonPath = "F:\\test\\json.txt";   // 検証するJSONファイル
+            string schemaPath = "C:/test/test.txt"; // JSON schema
+            string jsonPath = "C:/test/json.txt";   // 検証するJSONファイル
 
-            JsonValidator sample = new JsonValidator();
+            JsonValidator validator = new JsonValidator(schemaPath, jsonPath);
 
-            // ファイル読み込み
-            sample.SetSchema(schemaPath);
-            sample.SetJson(jsonPath);
+            switch (validator.Run())
+            {
+                case Status.Valid:
+                    Console.WriteLine("JSON is Valid");
+                    break;
 
-            sample.Run();
+                case Status.Invalid:
+                    Console.WriteLine("JSON is Invalid");
+                    break;
 
+                case Status.JsonError:
+                    Console.WriteLine("Can't parse JSON");
+                    break;
+
+                case Status.SchemaError:
+                    Console.WriteLine("Can't parse JSON Schema");
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
         }
     }
 }
