81afe892b736331004b00a7a7c915fdaf9b0fda3
ukai <ukai@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-10-28 08:05:04 +0000

    リファクタリング

---
+++ others/json-validator-ukai/JSON_Validator/JSON_Validator/src/CUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSON_Validator.Validator;
+
+
+namespace JSON_Validator
+{
+    class CUI
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string my_schema = "../../resource/JSON_Schema1.txt";
+                string JSON_input = "../../resource/JSON_input1.txt";
+
+                Validator.Result result = Validator.validatePath(my_schema, JSON_input);
+                switch (result.type)
+                {
+                    case Validator.Result.Type.Valid:
+                        Console.WriteLine("Your input data is valid\n");
+                        break;
+                    case Validator.Result.Type.Invalid:
+                        Console.WriteLine("Your input data is invalid\n");
+                        break;
+                    case Validator.Result.Type.IllFormed:
+                        Console.WriteLine("Your input data is ill-formed\n");
+                        Console.WriteLine(result.message);
+                        break;
+                    default:
+                        Console.Write("Unexpected error was occured\n");
+                        break;
+                }
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+    }
+}
---
+++ others/json-validator-ukai/JSON_Validator/JSON_Validator/src/Validator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace JSON_Validator
+{
+    class Validator
+    {
+
+        public struct Result
+        {
+            public enum Type
+            {
+                Valid,
+                Invalid,
+                IllFormed
+            }
+            readonly public Type type;
+            readonly public string message;
+
+            public Result(Type type, string message) {
+                this.type = type;
+                this.message = message;
+            }
+            public Result(Type type) {
+                this.type = type;
+                this.message = "";
+            }
+        }
+
+        
+        static private T open<T>(string  path, Func<StreamReader,T> f){
+             //定義したJSONスキーマの読込み
+             StreamReader sr = new StreamReader(
+                     path, Encoding.GetEncoding("Shift_JIS"));
+               T res;
+                try
+                {
+                   res = f(sr);
+
+                }finally{
+                    sr.Close();
+
+                }
+                  return res;
+        }
+
+        static public Result validatePath(string schemaPath, string jsonPath)
+        {
+            string schema = open(schemaPath, sr => { return sr.ReadToEnd(); });
+            string json = open(jsonPath, sr => { return sr.ReadToEnd(); });
+            return validate(schema, json);
+        }
+
+
+        static public Result validate(string schemaContent, string json) {
+            JsonSchema schema = JsonSchema.Parse(schemaContent);
+           
+            try
+            {
+                JObject jsonObj = JObject.Parse(json);
+                if (jsonObj.IsValid(schema))
+                {
+                    return new Result(Result.Type.Valid);
+                }
+                else
+                {
+                    return new Result(Result.Type.Invalid);
+                }
+            }catch(JsonReaderException e){
+                 return new Result(Result.Type.IllFormed,e.Message);
+            }
+        }
+    }
+}
