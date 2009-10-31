07fe391a34919cb1afe49918d41dd61ff821c8e7
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-10-31 16:02:41 +0000

    １．フォームを複数使用することを考慮して、ValidatorGUI->ValidatorMainFormに名前変更 ２．プログラム追加 a.テキストボックスの横の[add]ボタンをクリックするとファイルダイアログを開くようにした。 b.ファイルダイアログで選択したファイルのパスをテキストボックスに入力するようにした。

--- others/json-validator-ichihara/JSON_Validator/ValidatorMainForm.Designer.cs
+++ others/json-validator-ichihara/JSON_Validator/ValidatorMainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JSON_Validator
 {
-    partial class ValidatorGUI
+    partial class ValidatorMainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -34,17 +34,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog1.FilterIndex = 2;
+            this.openFileDialog1.Title = "Schema file selection";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(205, 61);
+            this.button1.Location = new System.Drawing.Point(205, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -85,28 +90,48 @@
             this.textBox2.Size = new System.Drawing.Size(174, 19);
             this.textBox2.TabIndex = 4;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "check";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(205, 110);
+            this.button2.Location = new System.Drawing.Point(205, 112);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
+            this.button2.TabIndex = 7;
             this.button2.Text = "add";
-            this.button2.UseCompatibleTextRendering = true;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // ValidatorGUI
+            // openFileDialog2
             // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog2.FilterIndex = 2;
+            this.openFileDialog2.Title = "JSON file selection";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // ValidatorMainForm
+            // 
+            this.AcceptButton = this.button3;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 266);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "ValidatorGUI";
+            this.Name = "ValidatorMainForm";
             this.Text = "JSON Validator - GUI ver";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -121,7 +146,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
 
     }
 }
---
+++ others/json-validator-ichihara/JSON_Validator/ValidatorMainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JSON_Validator
+{
+    public partial class ValidatorMainForm : Form
+    {
+        public ValidatorMainForm()
+        {
+            InitializeComponent();
+        }
+
+        /* コントロールをダブルクリックして生成された文をそのまま使用 */
+        /* わかりやすいように名前を変えたほうが良い？　　　　　　　　 */
+
+        // Shemaファイル入力ダイアログからの値入力
+        // テキストボックスにファイルパスを入力するのみ
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            this.Activate();
+
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+
+        // JSONファイル入力ダイアログからの値入力
+        // テキストボックスにファイルパスを入力するのみ
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            this.Activate();
+
+            textBox2.Text = openFileDialog2.FileName;
+        }
+
+        // Schemaファイル名をテキストボックスに入力する際に
+        // ファイル入力ダイアログを使用する場合のダイアログ起動ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        // JSONファイル名をテキストボックスに入力する際に
+        // ファイル入力ダイアログを使用する場合のダイアログ起動ボタン
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        // Validate開始ボタン
+        private void button3_Click(object sender, EventArgs e)
+        {
+            JsonValidator validator = new JsonValidator(textBox1.Text, textBox2.Text);
+
+
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new ValidatorMainForm());
+        }
+
+
+    }
+}
