6e9fa91504ec9ddffae9f4cc734dd83577eb3fd3
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2009-11-01 17:55:57 +0000

    タブ移動時におかしなことになるのでAcceptButtonを無効化

--- others/json-validator-ichihara/JSON_Validator/ValidatorMainForm.Designer.cs
+++ others/json-validator-ichihara/JSON_Validator/ValidatorMainForm.Designer.cs
@@ -120,16 +120,15 @@
             // 
             // ValidatorMainForm
             // 
-            this.AcceptButton = this.CheckButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 228);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.JsonAddButton);
             this.Controls.Add(this.CheckButton);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.SchemaAddButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
