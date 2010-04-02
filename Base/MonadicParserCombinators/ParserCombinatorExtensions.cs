8681c10bf7e51a241c44e729aef27cf17906cdc8
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-04-02 08:02:48 +0000

    再利用可能なものを追加

---
+++ Base/MonadicParserCombinators/ParserCombinatorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NU.OJL.MPRTOS.TLV.Base.FileContext.MonadicParserCombinators
+{
+    /*
+     * ＜参考＞
+     * LukeH's WebLog : Monadic Parser Combinators using C# 3.0
+     * http://blogs.msdn.com/lukeh/archive/2007/08/19/monadic-parser-combinators-using-c-3-0.aspx
+     */
+
+    public static class ParserCombinatorExtensions
+    {
+        public static Parser<TInput, TValue> OR<TInput, TValue>(
+        this Parser<TInput, TValue> parser1,
+        Parser<TInput, TValue> parser2)
+        {
+            return input => parser1(input) ?? parser2(input);
+        }
+        public static Parser<TInput, TValue2> AND<TInput, TValue1, TValue2>(
+            this Parser<TInput, TValue1> parser1,
+            Parser<TInput, TValue2> parser2)
+        {
+            return input => parser2(parser1(input).Rest);
+        }
+
+    }
+}
