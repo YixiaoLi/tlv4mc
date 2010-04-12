8cdbefb654c3beb84e074f7767fe9102f3b893d8
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-04-12 10:41:50 +0000

    クラス関係を修正

--- branches/refactoring_TraceLog/Core/FileContext/TraceLogData/TraceLogParser/INullObjectForParser.cs
+++ branches/refactoring_TraceLog/Core/FileContext/TraceLogData/TraceLogParser/INullObjectForParser.cs
@@ -5,8 +5,12 @@ using System.Text;
 
 namespace NU.OJL.MPRTOS.TLV.Core
 {
-    public interface INullObjectOfParser
+    public interface INullObjectOfParser : IParser
     {
+        /// <summary>
+        /// 既にパースが成功しているかどうか。
+        /// 具体的には、ParserCombinatorsクラスのORメソッドを通った場合true。
+        /// </summary>
         bool Success { get; set; }
     }
 }
