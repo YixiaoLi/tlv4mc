adc03f68e5c0dbf4c58b13b142eaddbdabddff3d
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-08-04 03:18:38 +0000

    名前空間の変更

--- Base/Parser/NullObjectForParser.cs
+++ Base/Parser/NullObjectForParser.cs
@@ -39,7 +39,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace NU.OJL.MPRTOS.TLV.Base
+namespace NU.OJL.MPRTOS.TLV.Base.Parser
 {
     public abstract class NullObjectOfParser : IParser
     {
--- Core/FileContext/TraceLogData/TraceLogParser/ITraceLogParser.cs
+++ Core/FileContext/TraceLogData/TraceLogParser/ITraceLogParser.cs
@@ -39,6 +39,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NU.OJL.MPRTOS.TLV.Base;
+using NU.OJL.MPRTOS.TLV.Base.Parser;
 
 namespace NU.OJL.MPRTOS.TLV.Core
 {
