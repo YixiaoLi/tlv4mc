dd6f818fd354bb09ef091b968c95571b6224d2f9
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-10-27 20:34:00 +0000

    統計情報ファイルを統計情報別に生成するようにした（未テスト）

--- Core/FileContext/StatisticsData/StatisticsData.cs
+++ Core/FileContext/StatisticsData/StatisticsData.cs
@@ -6,6 +6,9 @@ using NU.OJL.MPRTOS.TLV.Base;
 
 namespace NU.OJL.MPRTOS.TLV.Core
 {
+    /// <summary>
+    /// 統計情報に関するデータ
+    /// </summary>
     public class StatisticsData : IJsonable<StatisticsData>
     {
         public GeneralNamedCollection<Statistics> Statisticses { get; private set; }
