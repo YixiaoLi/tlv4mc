ad7a0f0fc5b40546b4058e0700a03ed6c97cdbe4
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-10-27 20:35:08 +0000

    統計情報ファイルを統計情報別に生成するようにした（未テスト） の忘れ物

--- Core/FileContext/StatisticsData/Statistics.cs
+++ Core/FileContext/StatisticsData/Statistics.cs
@@ -10,10 +10,12 @@ namespace NU.OJL.MPRTOS.TLV.Core
     /// <summary>
     /// 統計情報を保持するクラス。統計情報ファイル(*.sta)として出力される。
     /// </summary>
-    public class Statistics : INamed
+    public class Statistics : INamed, IJsonable<Statistics>
     {
         private string _name = string.Empty;
-
+        /// <summary>
+        /// 統計情報名。統計生成ルールファイルのルール名が入る。
+        /// </summary>
         public string Name
         {
             get { return _name; }
@@ -24,8 +26,27 @@ namespace NU.OJL.MPRTOS.TLV.Core
                     DisplayName = value;
             }
         }
+        /// <summary>
+        /// 表示用の名前
+        /// </summary>
         public string DisplayName { get; set; }
+        /// <summary>
+        /// グラフの初期タイプ
+        /// </summary>
         public string DefaultType { get; set; }
+        /// <summary>
+        /// データ系列
+        /// </summary>
         public Series Series { get; set; }
+
+        public string ToJson()
+        {
+            return ApplicationFactory.JsonSerializer.Serialize(this);
+        }
+
+        public Statistics Parse(string statistics)
+        {
+            return ApplicationFactory.JsonSerializer.Deserialize<Statistics>(statistics);
+        }
     }
 }
