1485ed281a6f508cac299b8ecb8b869f8fc2c1bf
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-11-11 05:26:39 +0000

    DataGridViewでExplorerを再現。 ビューアを形まで完成。

---
+++ Core/Controls/Forms/StatisticsViewer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NU.OJL.MPRTOS.TLV.Core.Controls
+{
+    public partial class StatisticsViewer : Form
+    {
+        private Statistics _data = null;
+
+        public StatisticsViewer()
+        {
+            InitializeComponent();
+        }
+
+        public StatisticsViewer(Statistics data)
+            : this()
+        {
+            _data = data;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            //chart1.Titles[0].Text = _data.Setting.Title;
+            chart1.Series[0].Label = _data.Series.Label;
+            chart1.Series[0].ChartType = _data.Setting.DefaultType;
+
+            foreach (DataPoint p in _data.Series.Points)
+            {
+                var dp = new System.Windows.Forms.DataVisualization.Charting.DataPoint();
+                dp.AxisLabel = p.XLabel;
+                dp.XValue = p.XValue;
+                dp.YValues[0] = p.YValue;
+                if (p.Color != null)
+                {
+                    dp.Color = (Color)p.Color;
+                }
+                chart1.Series[0].Points.Add(dp);
+            }
+        }
+
+        private void StatisticsViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visible = false;
+        }
+    }
+}
