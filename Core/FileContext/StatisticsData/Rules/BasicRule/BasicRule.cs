6766603213f83f22302dc171ee32506fcd6dfbc5
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2010-11-25 12:29:02 +0000

    ビューアの「グラフタイプ」が動かないように修正 コメント付与 InputRuleでべた書きの方法を追加

--- Core/FileContext/StatisticsData/Rules/BasicRule/BasicRule.cs
+++ Core/FileContext/StatisticsData/Rules/BasicRule/BasicRule.cs
@@ -12,10 +12,25 @@ namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Rules
 
     public class BasicRule
     {
+        /// <summary>
+        /// 統計の取り方
+        /// </summary>
         public BasicRuleMethod Method { get; set; }
+        /// <summary>
+        /// 時間間隔
+        /// </summary>
         public double Interval { get; set; }
+        /// <summary>
+        /// 始点となるイベント
+        /// </summary>
         public BaseEvent From { get; set; }
+        /// <summary>
+        /// 終点となるイベント
+        /// </summary>
         public BaseEvent To { get; set; }
+        /// <summary>
+        /// 始点と終点が同じイベント
+        /// </summary>
         public BaseEvent When { get; set; }
     }
 
@@ -23,7 +38,7 @@ namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Rules
     {
         public string ResourceName { get; set; }
         public string ResourceType { get; set; }
-        public string AttributeName { get; set; }
+         public string AttributeName { get; set; }
         public string AttributeValue { get; set; }   // 必ずしも文字列とは限らないので要修正
         public string BehaviorName { get; set; }
         public string BehaviorArg { get; set; }
