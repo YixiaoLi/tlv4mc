dabeed80c74131ff55e21ca5224abb73f1de922e
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2011-03-04 18:44:53 +0000

    コメントの付加。 統計情報の生成における例外と統計情報生成ルールの誤りによる例外をわけて考えることで、トレーサビリティの向上を狙った。

--- branches/statistics/Core/FileContext/StatisticsData/Mode/BasicRule/FromToPair.cs
+++ branches/statistics/Core/FileContext/StatisticsData/Mode/BasicRule/FromToPair.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Rules
+namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Mode
 {
+    /// <summary>
+    /// 基本解析モードで対象とするFrom,Toの各リソース名の組のリスト
+    /// </summary>
     public class FromToPair
     {
         public string From { get; set; }
