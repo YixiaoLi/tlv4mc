9a858e3ffc2e0243d1568446b5b359632fc660a9
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2011-03-07 10:39:40 +0000

    RegexpRuleにあった複数のpublicクラスのファイル分割

---
+++ Core/FileContext/StatisticsData/Mode/RegexpRule/RegexpReplacePatternPairList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Mode
+{
+    /// <summary>
+    /// マッチさせる正規表現とマッチした場合のDataPointへの格納方法の組み合わせのリスト
+    /// <para></para>
+    /// <para>　Key: 正規表現</para>
+    /// <para>　Val: マッチした場合のDataPointの各プロパティへ格納するデータ(またはその方法(例：${置換}))</para>
+    /// </summary>
+    public class RegexpReplacePatternPairList : GeneralKeyedJsonableCollection<string, DataPointReplacePattern, RegexpReplacePatternPairList>
+    {
+    }
+}
