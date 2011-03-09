d93ec8db659c22134542d181230787fe10084f99
ichihara <ichihara@eaa9bad3-1b54-4b84-9a43-5f6ba101aebc>
2011-03-09 21:20:44 +0000

    ChartSetting: ルール誤りの例外を投げるようにした BasicEvent: コメント付与 asp_stats.res: テスト用からリリース用へ変更 ルールフォルダ以下: テスト用ファイルの除去などのリリース用へ変更 cpu_utilization.rb: fmpに応用できると思っていたが、自信がないので一応asp用ということにした

--- branches/statistics/Core/FileContext/StatisticsData/Mode/BasicRule/BaseEvent.cs
+++ branches/statistics/Core/FileContext/StatisticsData/Mode/BasicRule/BaseEvent.cs
@@ -6,13 +6,34 @@ using NU.OJL.MPRTOS.TLV.Base;
 
 namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Mode
 {
+    /// <summary>
+    /// 統計情報生成ルールのBasicRuleにおけるWhen、From、Toを表すクラス
+    /// </summary>
     public class BaseEvent
     {
+        /// <summary>
+        /// 対象とするリソース名のリスト
+        /// </summary>
         public List<string> ResourceNames { get; set; }
+        /// <summary>
+        /// 対象とするリソースタイプのリスト
+        /// </summary>
         public List<string> ResourceType { get; set; }
+        /// <summary>
+        /// 対象とする属性遷移イベントにおける属性名
+        /// </summary>
         public string AttributeName { get; set; }
-        public Json AttributeValue { get; set; }
+        /// <summary>
+        /// 対象とする属性遷移イベントにおける属性値
+        /// </summary>
+        public Json AttributeValue { get; set; }  // Jsonにすることで、数値、文字列など様々な値をルールファイルに記述可能になる
+        /// <summary>
+        /// 対象とする振る舞いイベントにおける振る舞い名
+        /// </summary>
         public string BehaviorName { get; set; }
+        /// <summary>
+        /// 対象とする振る舞いイベントにおける引数
+        /// </summary>
         public string BehaviorArg { get; set; }
 
         public BaseEvent()
@@ -25,6 +46,13 @@ namespace NU.OJL.MPRTOS.TLV.Core.FileContext.StatisticsData.Mode
             BehaviorArg = string.Empty;
         }
 
+        /// <summary>
+        /// 対象とするリソース名のリストを取得する<para></para>
+        /// ResourceNamesに加え、ResourceTypeで指定したリソースタイプに属するする
+        /// リソースの名前を含めたリストを取得する
+        /// </summary>
+        /// <param name="data">対象トレースログのリソースファイルデータ</param>
+        /// <returns>対象とするすべてのリソース名のList</returns>
         public List<string> GetResourceNameList(ResourceData data)
         {
             List<string> result = new List<string>();
